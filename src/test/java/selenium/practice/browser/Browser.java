e9d5d09023dcc7a4f3748d69ebdcc5623964c1bc
Dhamotharan Rudrakumar <dhamotharan.rudrakumar@felixsolutions.ai>
2024-02-27 19:47:25 +0530

    Browser initialization is done

---
+++ src/test/java/selenium/practice/browser/Browser.java
@@ -0,0 +1,16 @@
+package selenium.practice.browser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Browser {
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\felix\\eclipse-workspace\\selenium.practice\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver ();
+	driver.get("https://www.leafground.com/window.xhtml");
+	driver.manage().window().maximize();
+
+
+
+	}
+}
