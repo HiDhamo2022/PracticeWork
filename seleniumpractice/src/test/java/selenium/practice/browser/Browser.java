88f8b0118d526c9b673477279a1a262a5d92c6e5
HiDhamo2022 <dhamotharan1608@gmail.com>
2024-02-28 20:59:49 +0530

    pom modified

--- seleniumpractice/src/test/java/selenium/practice/browser/Browser.java
+++ seleniumpractice/src/test/java/selenium/practice/browser/Browser.java
@@ -5,10 +5,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Browser {
 public static void main(String[] args) {
+	
 	System.setProperty("webdriver.chrome.driver", "C:\\Users\\felix\\eclipse-workspace\\selenium.practice\\chromedriver.exe");
 	WebDriver driver = new ChromeDriver ();
 	driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 	driver.manage().window().maximize();
 	driver.quit();
+	//upto date
 	}
 }
